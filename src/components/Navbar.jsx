7b156b4c51c328629c5f481c642d781418180255
marcoadmartins7 <marcoadmartins7@gmail.com>
2022-07-20 14:52:08 +0100

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,6 @@
 import { useState } from "react";
 import Logo from "../assets/logo.png";
-import {
-  FaBars,
-  FaTimes,
-  FaGithub,
-  FaLinkedin,
-  FaFacebook,
-} from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { Link } from "react-scroll";
