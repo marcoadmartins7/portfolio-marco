4aa0589e9869412a7254b5c535fd926cdb81c4af
marcoadmartins7 <marcoadmartins7@gmail.com>
2022-07-20 02:06:58 +0100

    create navbar portfolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Logo from "../assets/logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+export const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} alt="Logo image" style={{ width: "50px" }} />
+      </div>
+      {/*menu */}
+
+      <ul className="hidden md:flex ">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Contact</li>
+      </ul>
+
+      {/*Hamburguer */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/*Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+
+      {/*Social items */}
+      <div className="hidden"></div>
+    </div>
+  );
+};
