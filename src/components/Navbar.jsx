398b35a651fe4e6d693804ac0def1c109ff6c67d
marcoadmartins7 <marcoadmartins7@gmail.com>
2022-07-20 15:34:26 +0100

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -92,7 +92,7 @@ export const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href=""
+              href="/"
             >
               Email <HiOutlineMail size={30} />
             </a>
@@ -100,7 +100,7 @@ export const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href=""
+              href="/"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
