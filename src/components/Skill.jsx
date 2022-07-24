ab9c2507775c5810365fee3d10ff7e1dc3ed2e5d
marcoadmartins7 <marcoadmartins7@gmail.com>
2022-07-24 16:38:57 +0100

    fix

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,7 +1,7 @@
 const Skill = ({ tech, techName }) => {
   return (
     <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-      <img className="w-20 mt-4 mx-auto" src={tech} />
+      <img className="w-20 mt-4 mx-auto" src={tech} alt="/" />
       <p className="my-4 font-bold text-2xl">{techName}</p>
     </div>
   );
